d98e1f4ab98e0fa040e3fa1d4b0e910baa5dc4f9
jbuan <jerome.buan.06@gmail.com>
2022-04-11 13:52:33 +0200

    debut ex02

---
+++ module_0/ex02/Account.cpp
@@ -0,0 +1,20 @@
+#include "Account.hpp"
+
+/*
+	Public :
+
+	static int	getNbAccounts( void );
+	static int	getTotalAmount( void );
+	static int	getNbDeposits( void );
+	static int	getNbWithdrawals( void );
+	static void	displayAccountsInfos( void );
+	static int      getNbAccounts( void );
+        static int      getTotalAmount( void );
+        static int      getNbDeposits( void );
+        static int      getNbWithdrawals( void );
+        static void     displayAccountsInfos( void );	
+
+	Private :
+
+        static void     _displayTimestamp( void );	
+*/
